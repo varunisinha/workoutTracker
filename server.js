bbad3699364fc50e2126bd8d049d23fecb7fe3a3
varunisinha <varuni86@gmail.com>
2021-11-14 02:12:44 -0500

    adding the server file

---
+++ server.js
@@ -0,0 +1,29 @@
+// Pulling required files
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+// Listening on PORT 3000
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout",
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+// routes for api and html
+app.use(require("./routes/apiRoutings.js"));
+app.use(require("./routes/htmlRoutings.js"));
+
+app.listen(PORT, () => {
+  console.log(`App Listening on port ${PORT}!`);
+});
