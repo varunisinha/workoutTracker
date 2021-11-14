685e95ea3c24a24684b327a7de0eed8c42a5c198
varunisinha <varuni86@gmail.com>
2021-11-14 02:13:37 -0500

    adding routes folder

---
+++ routes/apiRoutings.js
@@ -0,0 +1,62 @@
+// Redirecting to workouts using API
+const router = require("express").Router();
+const Workout  = require("../models-folder/exercisePlan.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration:{$sum: "$exercises.duration"}
+      }
+    }
+  ]) 
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log("check!", body, params);
+  
+  Workout.findByIdAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).res.json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("ok");
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([{
+    $addFields: {
+      totalDuration:{$sum: "$exercises.duration"}
+    }
+  }])
+    .sort({_id:-1}).limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+module.exports = router;
